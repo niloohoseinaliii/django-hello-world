4efaad7a7651d2caa951415c8b9c1a503e4842ea
niloohoseinaliii <niloohoseinaliii@gmail.com>
2024-10-30 10:26:40 +0000

    wip

--- helloworld/views.py
+++ helloworld/views.py
@@ -7,29 +7,4 @@ from .forms import HelloWorldForm
 
 
 def index(request):
-    # retriever counter model instance from DB or create it if it doesn't exist yet
-    counter, created = Counter.objects.get_or_create(name='hello-world-button')
-
-    # increment counter when a POST request arrives (from the button click)
-    if request.method == 'POST':
-        counter.value += 1
-        counter.save()
-
-        # and get the values filled in form
-        form = HelloWorldForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            date = form.cleaned_data['date']
-
-    else:
-        form = HelloWorldForm()
-        username = 'Nobody'
-        date = datetime.date.today()
-
-    context = {
-        'clicks': counter.value,
-        'form': form,
-        'username': username,
-        'date': date,
-    }
-    return render(request, 'helloworld/index.html', context)
+    return render(request, 'helloworld/index.html')
